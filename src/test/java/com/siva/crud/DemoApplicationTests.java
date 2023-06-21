279d186cf38c9de980e58ee031dd490a2e04e65c
unvrknw881 <shivanagireddyrockstar@gmail.com>
2023-06-21 22:02:00 +0530

    testcases file is also uploaded to github

--- src/test/java/com/siva/crud/DemoApplicationTests.java
+++ src/test/java/com/siva/crud/DemoApplicationTests.java
@@ -3,10 +3,12 @@ package com.siva.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//junit testcases
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
+	
 	void contextLoads() {
 	}
 
